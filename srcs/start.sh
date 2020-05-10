9e9c4f022ffca768050e3e1586f024b9bf11c1be
slabelle42 <56270385+slabelle42@users.noreply.github.com>
2020-05-10 20:52:43 +0200

    Update start.sh

--- srcs/start.sh
+++ srcs/start.sh
@@ -1,7 +1,7 @@
 mkdir ~/mkcert
 cd ~/mkcert
 wget https://github.com/FiloSottile/mkcert/releases/download/v1.1.2/mkcert-v1.1.2-linux-amd64
-mv mkcert-v1.1.2-linux-amd64 mkcert && \
+mv mkcert-v1.1.2-linux-amd64 mkcert
 chmod +x mkcert
 ./mkcert -install
 ./mkcert localhost
