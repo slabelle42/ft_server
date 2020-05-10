70de4bc7e5e78f52c46acd5001ec448028a06cca
slabelle42 <56270385+slabelle42@users.noreply.github.com>
2020-05-10 20:50:34 +0200

    Create start.sh

---
+++ srcs/start.sh
@@ -0,0 +1,10 @@
+mkdir ~/mkcert
+cd ~/mkcert
+wget https://github.com/FiloSottile/mkcert/releases/download/v1.1.2/mkcert-v1.1.2-linux-amd64
+mv mkcert-v1.1.2-linux-amd64 mkcert && \
+chmod +x mkcert
+./mkcert -install
+./mkcert localhost
+
+service nginx restart
+tail -f /dev/null
