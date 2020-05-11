4eb44793038574b42a18014dbb2258ea20b9064d
slabelle42 <56270385+slabelle42@users.noreply.github.com>
2020-05-11 11:16:05 +0200

    Update start.sh

--- srcs/start.sh
+++ srcs/start.sh
@@ -11,7 +11,7 @@ echo "CREATE DATABASE wordpress;" | mysql -u root
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost';" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
 echo "update mysql.user set plugin = 'mysql_native_password' where user='root';" | mysql -u root
-mysql wordpress -u root < /var/www/html/wordpress.sql
+mysql wordpress -u root < /var/www/wordpress.sql
 
 service nginx restart
 service mysql restart
